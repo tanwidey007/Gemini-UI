76fc6ec4a970cf7de19da101d1d8d281386731d1
Tanwi Dey <155937309+tanwidey007@users.noreply.github.com>
2024-07-04 09:37:35 +0530

    Add files via upload

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,54 @@
+//import React from 'react'
+import { assets } from '../../assets/assets'
+import './Main.css'
+
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt=''/>
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p><span>Hello Tanwi,</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt=''/>
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: Urban Planning</p>
+            <img src={assets.bulb_icon} alt=''/>
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt=''/>
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt=''/>
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type='text' placeholder='enter a prompt here'/>
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so double-check its responses. your privacy and Gemini apps
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
